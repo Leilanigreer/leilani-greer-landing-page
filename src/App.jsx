1923fbe8093ad2978bd6986883e90337f0f88eb5
Leilanigreer <leilani@lpcgolf.com>
2024-11-26 14:19:10 -0800

    file cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import LandingPageOriginal from "./LandingPageOriginal";
 import { Projects } from "./Projects";
-import LandingPage2 from "./LandingPage2";
 import LandingPage from "./LandingPage";
-import LandingPage5 from "./LandingPage5";
 import AboutMe from "./components/AboutMe";
-import AboutMe2 from "./components/AboutMe";
 
 
 const router = createBrowserRouter([
@@ -23,18 +19,6 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <LandingPage />,
-      },
-      {
-        path: "/original",
-        element: <LandingPageOriginal />,
-      },
-      {
-        path: "/2",
-        element: <LandingPage2 />,
-      },
-      {
-        path: "/5",
-        element: <LandingPage5 />,
       },
       {
         path: "/AboutMe",
